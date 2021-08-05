1d40bef3f14294bd25d0221456954499f0ecc64c
Mugabo Uwilingiyimana <thierry.uwi@gmail.com>
2021-08-05 17:02:23 -0400

    code that pushed alumni data to atlas db

---
+++ Server/Models/alumnModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const AlumnSchema = new mongoose.Schema({
+  Session: String,
+  Age: Number,
+  latlong: [Number, Number],
+  Address: {
+    streetaddress: String,
+    streetaddressline2: String,
+    City: String,
+    State: String,
+    Zipcode: String,
+    country: String,
+  },
+})
+
+const Alumn = mongoose.model('Alumn', AlumnSchema)
+module.exports = Alumn

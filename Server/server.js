71ebc26a0ff2841d289ae8525aa94a59efc8c8f5
Mugabo Uwilingiyimana <thierry.uwi@gmail.com>
2021-08-06 10:04:54 -0400

    fix pointer to top level director, replacing __dirname

--- Server/server.js
+++ Server/server.js
@@ -1,32 +1,29 @@
-
 require('dotenv').config()
-const express = require('express');
+const path = require('path')
+const express = require('express')
 
 const app = express()
 
-
-
 let port = process.env.PORT || 5000
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
+const toplevelDir = path.resolve('.')
+// console.log('toplevel', toplevelDir)
 
-
-
-app.get('/', (req, res) =>{
-    res.sendFile(__dirname + '/client/public/index.html')
+app.get('/', (req, res) => {
+  res.sendFile(toplevelDir + '/client/public/index.html')
 })
 
-app.get('/lat', (req, res) =>{
-    res.sendFile(__dirname + "/client/public/lat.json")
+app.get('/lat', (req, res) => {
+  res.sendFile(toplevelDir + '/client/public/lat.json')
 })
 
-app.get('/lng', (req, res) =>{
-    res.sendFile(__dirname + "/client/public/lng.json")
+app.get('/lng', (req, res) => {
+  res.sendFile(toplevelDir + '/client/public/lng.json')
 })
 
-
-app.listen(port, () =>{
-    console.log("listening on port" + port)
+app.listen(port, () => {
+  console.log('listening on port' + port)
 })

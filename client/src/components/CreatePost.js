a6b412e4017e5b06c2940edadaf4ec8e69033472
Mugabo Uwilingiyimana <thierry.uwi@gmail.com>
2021-08-10 13:50:21 -0400

    ability to save post to db

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,51 +1,79 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { useAuth } from "../context/AuthContext";
-import { db } from "../firebase";
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { useAuth } from '../context/AuthContext'
+import { db } from '../firebase'
 
 const CreatePost = ({ handleClosePostModal }) => {
-  const [firstName, setFirstName] = useState(null);
-  const [lastName, setLastName] = useState(null);
-  const { currentUser } = useAuth();
+  const [firstName, setFirstName] = useState(null)
+  const [lastName, setLastName] = useState(null)
+  const [error, setError] = useState('')
+  const { currentUser } = useAuth()
 
   useEffect(() => {
-    db.collection("users")
-      .where("email", "==", `${currentUser.email}`)
+    db.collection('users')
+      .where('email', '==', `${currentUser.email}`)
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          setFirstName(doc.data().firstName);
-          setLastName(doc.data().lastName);
-        });
-      });
-  }, [currentUser.email]);
+          setFirstName(doc.data().firstName)
+          setLastName(doc.data().lastName)
+        })
+      })
+  }, [currentUser.email])
 
-  function handlePostSubmit(e) {
-    e.preventDefault();
-    console.log(e.target.body.value);
-    console.log(e.target.category.selectedOptions)
+  async function handlePostSubmit(e) {
+    e.preventDefault()
+
+    // grab body message
+    let body = e.target.body.value
+
+    // create tags array
+    let options = e.target.category.selectedOptions
+    let tags = []
+    Array.from(options).forEach((option) => tags.push(option.value))
+
+    // reset error
+    setError('')
+
+    // add user to the posts collection
+    try {
+      await db.collection('posts').add({
+        body: body,
+        category: tags,
+        Image: false,
+        user: {
+          email: currentUser.email,
+          firstName: firstName,
+          lastName: lastName,
+        },
+      })
+    } catch (err) {
+      setError('Sorry, please try again.')
+      console.log(err)
+    }
   }
 
   return (
-    <div id="createPost">
+    <div id='createPost'>
       {firstName && <h3>{firstName}</h3>}
       {lastName && <h3>{lastName}</h3>}
-      <form id="createPostForm" onSubmit={handlePostSubmit}>
+      <form id='createPostForm' onSubmit={handlePostSubmit}>
         <textarea
-          id="postBody"
-          type="text"
-          name="body"
-          placeholder="Type your description here..."
+          id='postBody'
+          type='text'
+          name='body'
+          placeholder='Post message...'
         ></textarea>
-        <select id="categoryMenu" name="category">
+        <select multiple='multiple' id='categoryMenu' name='category'>
           <option>Categories:</option>
-          <option value="Tiny Houses">Tiny Houses</option>
-          <option value="Tree Houses">Tree Houses</option>
+          <option value='Tiny Houses'>Tiny Houses</option>
+          <option value='Tree Houses'>Tree Houses</option>
         </select>
-        <input type="submit" id="postButton" value="Create Post"/>
+        <input type='submit' id='postButton' value='Create Post' />
       </form>
+      {error && error}
     </div>
-  );
-};
+  )
+}
 
-export default CreatePost;
+export default CreatePost

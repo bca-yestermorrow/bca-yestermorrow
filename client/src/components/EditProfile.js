d146a1ca379fc59250b2cae30d68e71abbe8a079
Ian French <ianfrench33@gmail.com>
2021-08-17 10:19:41 -0400

    fixed location bug and added comments

--- client/src/components/EditProfile.js
+++ client/src/components/EditProfile.js
@@ -1,8 +1,10 @@
 import React from "react";
+// import useAuth to get current user id
 import { useAuth } from "../context/AuthContext";
 import { db } from "../firebase";
 import { useState, useEffect } from "react";
 import ProfilePicture from "./ProfilePicture";
+// imports for material ui
 import {
   Card,
   Container,
@@ -11,10 +13,10 @@ import {
   Select,
   MenuItem,
   Input,
-  Avatar
+  Avatar,
 } from "@material-ui/core";
+// import for material ui to customize styles
 import { makeStyles } from "@material-ui/core/styles";
-
 
 const EditProfile = ({ handleModalClosed }) => {
   const [user, setUser] = useState("");
@@ -23,34 +25,40 @@ const EditProfile = ({ handleModalClosed }) => {
   const [imageURL, setImageURL] = useState("");
   const { currentUser } = useAuth();
   let categoryArray = [];
+  // allows use of various customized styles on material ui components
   const useStyles = makeStyles({
+    // to be used for larger profile pics/avatars
     large: {
       width: "200px",
       height: "200px",
-      fontSize: "100px"
+      fontSize: "100px",
     },
+    // to be used for smaller profile pics/avatars
     small: {
       width: "50px",
       height: "50px",
-      fontSize: "25px"
+      fontSize: "25px",
     },
+    // button color
     green: {
-      backgroundColor: "#59833b"
-    }
-  })
-
-  const classes = useStyles()
+      backgroundColor: "#59833b",
+    },
+  });
+  // allows use of classes.whatever on mui components
+  const classes = useStyles();
 
   const getImageURL = (url) => {
     setImageURL(url);
   };
-  console.log(imageURL);
+  // function to get the user document of the current user from the database
   const getCurrentUser = async () => {
+    // from the users collection, get the doc with the id of currentuser.uid
     await db
       .collection("users")
       .doc(currentUser.uid)
       .get()
       .then((doc) => {
+        // if there is a doc with this id
         if (doc.exists) {
           // doc.data() is never undefined for query doc snapshots
           console.log(user);
@@ -64,7 +72,7 @@ const EditProfile = ({ handleModalClosed }) => {
         console.log("Error getting documents: ", error);
       });
   };
-
+  // function to get the list of categories from the database
   const getCategories = async () => {
     await db
       .collection("categories")
@@ -72,12 +80,14 @@ const EditProfile = ({ handleModalClosed }) => {
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
           console.log(doc.id, " => ", doc.data());
+          // push each category/doc to an array
           categoryArray.push(doc.data());
         });
       });
+    // setCategories to the array of docs to be used in the form dropdown
     setCategories(categoryArray);
   };
-
+  // function to handle form submit. updates user doc with new information
   const handleSubmit = async (evt) => {
     evt.preventDefault();
     // let userCity = evt.target.city.value;
@@ -89,7 +99,7 @@ const EditProfile = ({ handleModalClosed }) => {
     let userPortfolio = evt.target.portfolio.value;
     let userCity = evt.target.city.value;
     let userState = evt.target.state.value;
-    let userCountry = evt.target.country.value
+    let userCountry = evt.target.country.value;
     let userClasses = categoryName;
 
     let userProfile = await db
@@ -99,65 +109,51 @@ const EditProfile = ({ handleModalClosed }) => {
       .then((doc) => {
         // doc.data() is never undefined for query doc snapshots
         //we are doing this twice, here and getCurrentUser
+        // each if statement is separate so the database isnt updated with empty values
         if (doc.exists) {
-          if (userFirstName){
-            doc.ref.update({firstName: userFirstName})
+          // if userFirstName, the user input value, is true, update the user doc
+          if (userFirstName) {
+            doc.ref.update({ firstName: userFirstName });
           }
           if (userLastName) {
             doc.ref.update({
-              lastName: userLastName
-            })
+              lastName: userLastName,
+            });
           }
           if (userBio) {
             doc.ref.update({
-              bio: userBio
-            })
+              bio: userBio,
+            });
           }
           if (userProjects) {
-            doc.ref.update({projects: userProjects})
+            doc.ref.update({ projects: userProjects });
           }
           if (userPortfolio) {
-            doc.ref.update({portfolio: userPortfolio})
+            doc.ref.update({ portfolio: userPortfolio });
           }
+          // categoryName is initialized as an empty array, so only update if it isnt empty
           if (categoryName !== []) {
-            doc.ref.update({classes: categoryName})
+            doc.ref.update({ classes: categoryName });
           }
           if (imageURL) {
-            doc.ref.update({profilePic: imageURL})
+            doc.ref.update({ profilePic: imageURL });
           }
           if (userCity) {
             doc.ref.update({
-              location: {
-                city: userCity
-              }
-            })
+              "location.city": userCity
+            });
           }
           if (userState) {
             doc.ref.update({
-              location: {
-                state: userState
-              }
-            })
+              "location.state": userState
+            });
           }
           if (userState) {
             doc.ref.update({
-              location: {
-                country: userCountry
-              }
-            })
+              "location.country": userCountry
+            });
           }
-            // doc.ref.update({
-            // firstName: !userFirstName ? user.firstName : userFirstName,
-            // lastName: !userLastName ? user.lastName : userLastName,
-            // location: {
-            // city: !userCity ? user.location.city : userCity,
-            // state: !userState ? user.location.state : userState,
-            // },
-            // bio: !userBio ? user.bio : userBio,
-            // projects: !userProjects ? user.projects : userProjects,
-            // classes: !categoryName ? user.classes : categoryName,
-            // profilePic: !imageURL ? user.profilePic : imageURL,
-          // });
+
           setUser(doc.data());
         } else {
           console.log("no document");
@@ -167,11 +163,10 @@ const EditProfile = ({ handleModalClosed }) => {
       .catch((error) => {
         console.log("Error getting documents: ", error);
       });
+    // after updating, call getcurrentuser to get updated info, reset categoryname and close modal
     getCurrentUser();
     setCategoryName([]);
-    evt.target.bio.value = ""
-
-    handleModalClosed()
+    handleModalClosed();
   };
 
   const handleChange = (evt) => {
@@ -189,109 +184,117 @@ const EditProfile = ({ handleModalClosed }) => {
   }, []);
   console.log(categoryName);
   console.log(categories);
+  console.log(user.location)
   return (
     <div className="edit-profile-container">
-      
       <div className="form-container">
-          <form
-            className="edit-profile-form"
-            onSubmit={handleSubmit}
-            autoComplete="off"
-          >
-            <button onClick={handleModalClosed} className="x-button">X</button>
-            <label for="profile-firstName">First Name: </label>
-            <TextField
-              className="input-field"
-              id="profile-firstName"
-              label={user.firstName}
-              name="firstName"
-              variant="filled"
-            />
+        <form
+          className="edit-profile-form"
+          onSubmit={handleSubmit}
+          autoComplete="off"
+        >
+          <button onClick={handleModalClosed} className="x-button">
+            X
+          </button>
+          <label for="profile-firstName">First Name: </label>
+          <TextField
+            className="input-field"
+            id="profile-firstName"
+            label={user.firstName}
+            name="firstName"
+            variant="filled"
+          />
 
-            <label for="profile-lastName">Last Name: </label>
-            <TextField
-              className="input-field"
-              id="profile-lastName"
-              label={user.lastName}
-              name="lastName"
-              variant="filled"
-            />
-            <TextField
-            id="profile=city"
-            // label={user.location.city}
+          <label for="profile-lastName">Last Name: </label>
+          <TextField
+            className="input-field"
+            id="profile-lastName"
+            label={user.lastName}
+            name="lastName"
+            variant="filled"
+          />
+          <label for="profile-city">City: </label>
+          <TextField
+          className="input-field"
+            id="profile-city"
+            label={user.location ? user.location.city : "city"}
             name="city"
             variant="filled"
           />
+          <label for="profile-state">State: </label>
           <TextField
+          className="input-field"
             id="profile-state"
-            // label={user.location.state}
+            label={user.location ? user.location.state : "state"}
             name="state"
             variant="filled"
           />
+          <label for="profile-country">Country: </label>
           <TextField
+          className="input-field"
             id="profile-country"
-            // label={user.location.state}
+            label={user.location ? user.location.country : "country"}
             name="country"
             variant="filled"
           />
-            <label for="profile-interests">Interests: </label>
-            <Select
-              className="input-field"
-              id="profile-interests"
-              onChange={handleChange}
-              value={categoryName}
-              input={<Input />}
-              name="classes"
-              multiple
-            >
-              {categories &&
-                categories.map((category) => {
-                  return (
-                    <MenuItem key={category.name} value={category.name}>
-                      {category.name}
-                    </MenuItem>
-                  );
-                })}
-            </Select>
+          <label for="profile-interests">Interests: </label>
+          <Select
+            className="input-field"
+            id="profile-interests"
+            onChange={handleChange}
+            value={categoryName}
+            input={<Input />}
+            name="classes"
+            multiple
+          >
+            {categories &&
+              categories.map((category) => {
+                return (
+                  <MenuItem key={category.name} value={category.name}>
+                    {category.name}
+                  </MenuItem>
+                );
+              })}
+          </Select>
 
-            <label for="profile-bio">Bio: </label>
-            <TextField
-              className="input-field"
-              id="profile-bio"
-              label={user.bio}
-              name="bio"
-              variant="filled"
-            />
+          <label for="profile-bio">Bio: </label>
+          <TextField
+            className="input-field"
+            id="profile-bio"
+            label={user.bio}
+            name="bio"
+            variant="filled"
+          />
 
-            <label for="profile-projects">Projects: </label>
-            <TextField
-              className="input-field"
-              id="profile-projects"
-              label={user.projects}
-              name="projects"
-              variant="filled"
-            />
-            <label for="profile-portfolio">Portfolio/Social links: </label>
-            <TextField
-              className="input-field"
-              id="profile-portfolio"
-              label={user.portfolio}
-              name="portfolio"
-              variant="filled"
-            />
-            <label for="profile-picture">Upload a profile picture</label>
-            <ProfilePicture getImageURL={getImageURL} id="profile-picture" />
+          <label for="profile-projects">Projects: </label>
+          <TextField
+            className="input-field"
+            id="profile-projects"
+            label={user.projects}
+            name="projects"
+            variant="filled"
+          />
+          <label for="profile-portfolio">Portfolio/Social links: </label>
+          <TextField
+            className="input-field"
+            id="profile-portfolio"
+            label={user.portfolio}
+            name="portfolio"
+            variant="filled"
+          />
+          <label for="profile-picture">Upload a profile picture</label>
+          <ProfilePicture getImageURL={getImageURL} id="profile-picture" />
 
-            <Button
-              id="profile-submit"
-              className={classes.green}
-              color="green"
-              variant="contained"
-              type="submit"
-            >
-              Submit
-            </Button>
-          </form>
+          <Button
+            id="profile-submit"
+            className={classes.green}
+            color="green"
+            variant="contained"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </form>
       </div>
     </div>
   );

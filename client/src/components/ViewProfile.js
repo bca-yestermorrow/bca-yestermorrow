97d564d0c0dd2283deab72ee1adb1b8e57dc0414
Matt Nolan <menolan22@gmail.com>
2021-08-20 11:40:17 -0400

    profile styling

--- client/src/components/ViewProfile.js
+++ client/src/components/ViewProfile.js
@@ -3,7 +3,15 @@ import { useAuth } from "../context/AuthContext";
 import { db } from "../firebase";
 import { useState, useEffect } from "react";
 import firebase from "firebase";
-import { Avatar, Button, FormControl, Checkbox, FormGroup, FormControlLabel } from "@material-ui/core";
+import {
+  Avatar,
+  Button,
+  FormControl,
+  Checkbox,
+  FormGroup,
+  FormControlLabel,
+  Card,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import EditProfile from "./EditProfile";
 
@@ -30,6 +38,9 @@ const ViewProfile = () => {
         backgroundColor: "#59833b",
         color: "#fff",
       },
+      profileCard: {
+        height: "200px",
+      },
     },
   });
 
@@ -42,9 +53,8 @@ const ViewProfile = () => {
   const handleModalClosed = () => {
     setModal("");
     setTimeout(getProfile, 500);
-    console.log("in modal close")
+    console.log("in modal close");
   };
-
 
   const getProfile = async () => {
     let profileRef = await db
@@ -55,7 +65,6 @@ const ViewProfile = () => {
         if (doc.exists) {
           setProfile(doc.data());
           console.log(profile);
-          
         } else {
           console.log("No doc found");
         }
@@ -69,7 +78,7 @@ const ViewProfile = () => {
   }, []);
   console.log(profile.interests);
   return (
-    <div>
+    <div className="profile-page-wrapper">
       <img id="banner" src="../main_forum_banner.jpg" alt="alt" />
       {/* <div id="banner"></div> */}
       <div className="view-profile-page">
@@ -89,34 +98,45 @@ const ViewProfile = () => {
             <p className="user-full-name">
               {profile.firstName} {profile.lastName}
             </p>
-            {profile.location &&
-            <p className="user-location">
-              {profile.location.city}, {profile.location.state},{" "}
-              {profile.location.country}
-            </p>
-            }
+            {profile.location && (
+              <p className="user-location">
+                {profile.location.city}, {profile.location.state},{" "}
+                {profile.location.country}
+              </p>
+            )}
             <p className="user-links">{profile.portfolio}</p>
             <div className="user-work">
-              <div>
-                <h4>About Me</h4>
-                <p className="user-bio">{profile.bio}</p>
-              </div>
-              <div>
-                <h4>Projects</h4>
-                <p className="user-projects">{profile.projects}</p>
-              </div>
+              <Card>
+                <div>
+                  <h4>About Me</h4>
+                  <p className="user-bio">{profile.bio}</p>
+                </div>
+              </Card>
+              <Card className={classes.profileCard}>
+                <div>
+                  <h4>Projects</h4>
+
+                  <p className="user-projects">{profile.projects}</p>
+                </div>
+              </Card>
             </div>
             <div>
               <h4>Interests</h4>
-              {profile.interests ? ( profile.interests.map((interest, index) => {
-                return (
-                  <p className="user-interests" key={index}>{interest}</p>
-                );
-              }) ) : (
+              {profile.interests ? (
+                profile.interests.map((interest, index) => {
+                  return (
+                    <p className="user-interests" key={index}>
+                      {interest}
+                    </p>
+                  );
+                })
+              ) : (
                 <p></p>
               )}
             </div>
-            <Button className={classes.green} onClick={handleModalOpen}>Edit</Button>
+            <Button className={classes.green} onClick={handleModalOpen}>
+              Edit
+            </Button>
           </div>
         ) : (
           <p>loading...</p>

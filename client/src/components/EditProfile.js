eb08f1db2ba04a6935042845790ec9fe7c230ece
Ian French <ianfrench33@gmail.com>
2021-08-26 09:02:19 -0400

    fixed create profile modal styling and made 'states' a dropdown

--- client/src/components/EditProfile.js
+++ client/src/components/EditProfile.js
@@ -13,7 +13,6 @@ import {
   Select,
   MenuItem,
   Input,
-  Avatar,
 } from "@material-ui/core";
 // import for material ui to customize styles
 import { makeStyles } from "@material-ui/core/styles";

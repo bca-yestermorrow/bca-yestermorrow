5ae5b9ca35cb464dd8441f50426cb7f2be636166
Sam LaChapelle <sambosauce@gmail.com>
2021-10-06 14:27:27 -0400

    error page render error fixed

--- client/src/components/ErrorPage.js
+++ client/src/components/ErrorPage.js
@@ -6,7 +6,7 @@ const ErrorPage = () => {
 
     return (
         <div id="errorPage">
-            <h1 id="errorTitle">404 Error</h1>
+            <h1 id="errorTitle">404 Page Not Found</h1>
             <h2 className="errorMessage">Oops, this page doesn't seem to exist.</h2>
             <h2 className="errorMessage">Sorry for the inconvenience. Please click the button below to head back to the connect page... Thank you.</h2>
             <Link to="/connect">
